00a83f9094e96602fb1ffac59cbe8e19ae257abd
leotran@student.hive.fi <vanleotran@gmail.com>
2023-08-15 15:42:57 +0300

    fixed types in boxcontent

--- client/src/components/BoxContent/BoxContent.tsx
+++ client/src/components/BoxContent/BoxContent.tsx
@@ -3,6 +3,7 @@ import { Main, Title, ItemBox, Item } from './styles'
 import { useContext } from 'react';
 import { BoxComponentContext } from '../../context/BoxComponentContext';
 import { BoxComponentType } from '../../types/BoxComponentType';
+import { StringKeyObject } from '../../types/types';
 
 const BoxContent = () => {
 	const { category } = useContext(BoxComponentContext) as BoxComponentType;
@@ -10,14 +11,14 @@ const BoxContent = () => {
 
 	return	(
 		<Main>
-			{ category.titles.map((title: any, index: any) => {
+			{ category.titles.map((title: StringKeyObject<string>, index: number) => {
 				return (
 					<ItemBox key={title.name}>
 						<Title $index={index}>
 							{ index !== 5 && index !== 7 ? title.name : '' }<br/>
 							{ title.unit ? `(${title.unit})` : ''}
 						</Title>
-						{ category.products.map((item: any, i: any) => (
+						{ category.products.map((item: Array<string>, i: number) => (
 							index === lastIndex
 							? <BoxIcons key={i}/>
 							:	<Item key={i} $index={index}>{item[index]}</Item>
