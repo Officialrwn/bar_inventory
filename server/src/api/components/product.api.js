0fd722929515767f5ec2505a161468c86ec972db
leotran@student.hive.fi <vanleotran@gmail.com>
2024-04-05 22:52:58 +0300

    added get products

--- server/src/api/components/product.api.js
+++ server/src/api/components/product.api.js
@@ -1,7 +1,13 @@
 import express from 'express';
-import { checkProductType, addProductListItem, removeProductListItem } from '../../controllers/product.controller.js';
+import { checkProductType, getProductListItem, addProductListItem, removeProductListItem } from '../../controllers/product.controller.js';
 
 const app = express();
+
+app.get('/api/products', async (req, res) => {
+		const data = await getProductListItem();
+		console.log("DATA", data);
+		res.sendStatus(200).json({"TEST!!!!": data});
+})
 
 app.post('/api/products/add', async (req, res) => {
 	if (!req.body || !checkProductType(req.query.type)) {
@@ -9,7 +15,7 @@ app.post('/api/products/add', async (req, res) => {
 	} else {
 		const data = await addProductListItem(req.body, req.query.type);
 		console.log("DATA", data);
-		res.sendStatus(200);
+		res.sendStatus(200).json(data);
 	}
 })
 
